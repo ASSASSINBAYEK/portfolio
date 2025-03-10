fe627bf01871c65cd433ea25de3b1f69a565caf6
ASSASSINBAYEK <jatinhegde17@gmail.com>
2025-03-11 00:28:21 +0530

    scroll fix for mobile devices - 1

--- app.js
+++ app.js
@@ -1,70 +1,69 @@
 document.addEventListener("DOMContentLoaded", () => {
-    particlesJS("particles-js", {
-      particles: {
-        number: {
-          value: 80,
-          density: {
-            enable: true,
-            value_area: 800,
-          },
-        },
-        color: {
-          value: "#00ff00",
-        },
-        shape: {
-          type: "circle",
-        },
-        opacity: {
-          value: 0.6,
-          random: false,
-        },
-        size: {
-          value: 8,
-          random: true,
-        },
-        line_linked: {
+  particlesJS("particles-js", {
+    particles: {
+      number: {
+        value: 80,
+        density: {
           enable: true,
-          distance: 150,
-          color: "#00ff00",
-          opacity: 0.4,
-          width: 1,
-        },
-        move: {
-          enable: true,
-          speed: 4,
-          direction: "none",
-          random: false,
-          straight: false,
-          out_mode: "out",
-          bounce: false,
+          value_area: 800,
         },
       },
-      interactivity: {
-        detect_on: "window",
-        events: {
-          onhover: {
-            enable: true,
-            mode: "grab",
-          },
-          onclick: {
-            enable: true,
-            mode: "push",
-          },
-          resize: true,
+      color: {
+        value: "#00ff00",
+      },
+      shape: {
+        type: "circle",
+      },
+      opacity: {
+        value: 0.6,
+        random: false,
+      },
+      size: {
+        value: 8,
+        random: true,
+      },
+      line_linked: {
+        enable: true,
+        distance: 150,
+        color: "#00ff00",
+        opacity: 0.4,
+        width: 1,
+      },
+      move: {
+        enable: true,
+        speed: 4,
+        direction: "none",
+        random: false,
+        straight: false,
+        out_mode: "out",
+        bounce: false,
+      },
+    },
+    interactivity: {
+      detect_on: "window",
+      events: {
+        onhover: {
+          enable: !("ontouchstart" in window),
+          mode: "grab",
         },
-        modes: {
-          grab: {
-            distance: 200,
-            line_linked: {
-              opacity: 1,
-            },
+        onclick: {
+          enable: true,
+          mode: "push",
+        },
+        resize: true,
+      },
+      modes: {
+        grab: {
+          distance: 200,
+          line_linked: {
+            opacity: 1,
           },
-          push: {
-            particles_nb: 4,
-          },
+        },
+        push: {
+          particles_nb: 4,
         },
       },
-      retina_detect: true,
-    });
+    },
+    retina_detect: true,
   });
-  
+});
